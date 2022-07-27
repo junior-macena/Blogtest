711f356c0b8c5d3239c43418a415752f024d91fa
junior-macena <brjunior834@gmail.com>
2022-07-27 00:31:54 +0000

    css e html

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask ("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello World"
+posts = [
+    {
+        "title": "O meu primeiro post",
+        "body": "Aqui e o Texto do Post",
+        "author": "Junior",
+        "created": datetime(2022,7,25)
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+    },
+    {
+        "title": "O meu Segundo post",
+        "body": "Aqui e o Texto do Post",
+        "author": "Cesar",
+        "created": datetime(2022,7,25)
+    },
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
