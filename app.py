0a2e114cbf3e79d48e8b0808eb62a2a808b065c7
junior-macena <brjunior834@gmail.com>
2022-07-31 18:26:26 +0000

    atualiza-teste

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
-import os
 
 app = Flask("hello")
-db_url = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
-app.config["SQLALCHEMY_DATABASE_URI"] =  db_url.replace("postgres", "postgresql") 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "pudim"
 
@@ -26,9 +24,9 @@ class Post(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
-    email = db.Column(db.String(32), nullable=False, unique=True)
-    password_hash = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='author')
 
     def set_password(self, password):
@@ -46,10 +44,10 @@ db.create_all()
 
 @app.route("/")
 def index():
-    posts = Post.query.order_by(-Post.created).all()
-    return render_template("index.html", posts=posts)
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
 
-@app.route('/register', methods=["GET","POST"])
+@app.route("/register", methods=["GET" ,"POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -57,17 +55,17 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        try:
+        try:    
             new_user = User(username=username, email=email)
             new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
         except IntegrityError:
-            flash("Username or E-mail already exists!")
+            flash("Username or E-mail alredy exists!")
         else:
             return redirect(url_for('login'))
-    return render_template('register.html')
-
+    return render_template('register.html')    
+    
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
@@ -77,11 +75,11 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user is None or not user.check_password(password):
-            flash("Incorrect Username or Password")
+            flash('Incorrect Username or Password')
             return redirect(url_for('login'))
         login_user(user)
         return redirect(url_for('index'))
-    return render_template("login.html")
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
